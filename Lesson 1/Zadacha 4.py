e1996230527b3f80ec0ffdf570655ec75d0c759e
Ilya7598899 <114740651+Ilya7598899@users.noreply.github.com>
2022-12-18 23:42:49 +0300

    Сделал задачу 4

--- Lesson 1/Zadacha 4.py
+++ Lesson 1/Zadacha 4.py
@@ -1,5 +1,5 @@
 a = int(input('Введите номер четверти: '))
-if a != 1 or a != 2 or a != 3 or a != 4: print('Нет такой четверти')
+if a != 1 and a != 2 and a != 3 and a != 4: print('Нет такой четверти')
 if a == 1:
     print('x > 0, y > 0')
 if a == 2:
