32eb7a7a5b1663799d0e06b65a32987bcace42e9
Ilya <i7598899@gmail.com>
2022-12-30 16:27:46 +0300

    Сделал задачу 3

--- Lesson 4/Zadacha 3.py
+++ Lesson 4/Zadacha 3.py
@@ -21,14 +21,12 @@
 # [6, 2, 3, 0, 9]
 
 import random
-n = int(input("Введите количество чисел N: "))
-pr=[]
-spisok=[]
-for i in range(1,n+1):
-      spisok.append(random.randint(1,10))
-for i in range(1,len(spisok)-1):
-      pr.append(spisok[i-1] * spisok[-i])
-if n%2 !=0:
-    pr[int(n/2)]=spisok[int(n/2)]
-print(spisok)
-print(pr)
+n = int(input("Введите количество чисел в списке: "))
+pr = []
+spisok = []
+for i in range(1, n+1):
+    spisok.append(random.randint(1, 10))
+print("Изначальный список: ", spisok)
+mySet = set(spisok) 
+print("Список без повторов", mySet)
+
